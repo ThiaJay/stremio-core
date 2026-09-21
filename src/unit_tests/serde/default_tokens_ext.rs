32ccee5261b283a6247edff6737c1ce0957ab4bf
TeeJay <3618451+ThiaJay@users.noreply.github.com>
2026-09-21 11:58:50 +0100

    Update default tokens for Skip Intro mode

--- src/unit_tests/serde/default_tokens_ext.rs
+++ src/unit_tests/serde/default_tokens_ext.rs
@@ -409,8 +409,11 @@ impl DefaultTokens for Settings {
             },
             Token::Str("nextVideoNotificationDuration"),
             Token::U32(35000),
-            Token::Str("skipIntro"),
-            Token::Bool(true),
+            Token::Str("skipIntroMode"),
+            Token::UnitVariant {
+                name: "SkipIntroMode",
+                variant: "ask",
+            },
             Token::Str("audioPassthrough"),
             Token::Bool(false),
             Token::Str("audioLanguage"),
