65560ee436cdf1dff7a4c88ed6d62a31fd28f9ed
TeeJay <3618451+ThiaJay@users.noreply.github.com>
2026-09-22 05:42:00 +0100

    Serialize skip segment kinds consistently for clients

--- src/types/skip_segments.rs
+++ src/types/skip_segments.rs
@@ -2,6 +2,7 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub enum SkipSegmentKind {
     Intro,
     Recap,
